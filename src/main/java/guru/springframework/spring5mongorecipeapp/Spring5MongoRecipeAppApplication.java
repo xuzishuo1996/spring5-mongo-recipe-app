3239a4c0df06592571fc3d468707014bc1486222
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-09-07 19:50:53 +0800

    change embedded mongo back to test scope. add load UOM and Categories in bootstrap. error: 1. fail IT and app tests. 2. fail to update ingredients.

--- src/main/java/guru/springframework/spring5mongorecipeapp/Spring5MongoRecipeAppApplication.java
+++ src/main/java/guru/springframework/spring5mongorecipeapp/Spring5MongoRecipeAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class Spring5MongoRecipeAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring5MongoRecipeAppApplication.class, args);
     }
-
 }
